41c8026c79d6ae755d7f1456aa63c2bcead83ed5
Simon Wenk <simon.wenk@igd.fraunhofer.de>
2024-03-19 11:28:34 +0200

    added console reading and writing

--- manifest.ts
+++ manifest.ts
@@ -1,36 +1,36 @@
 import { type ManifestV3Export } from '@crxjs/vite-plugin'
 
-// Since Firefox does not support the full ManifestV3 spec, 
+// Since Firefox does not support the full ManifestV3 spec,
 // we need to use different properties for the background script
 let background = {
-    type: 'module',
-    ...process.env.TARGET === 'chrome' && {
-        service_worker: 'src/background/index.ts',
-    },
-    ...process.env.TARGET === 'firefox' && {
-        scripts: ['src/background/index.ts'],
-    },
+  type: 'module',
+  ...(process.env.TARGET === 'chrome' && {
+    service_worker: 'src/background/index.ts',
+  }),
+  ...(process.env.TARGET === 'firefox' && {
+    scripts: ['src/background/index.ts'],
+  }),
 }
 
 export const manifest: ManifestV3Export = {
   manifest_version: 3,
   name: 'Tinkercad MQTT',
-  description: 'An extension, which can connect a serial console in Tinkercad to MQTT.',
-  version: "1.0.0",
+  description:
+    'An extension, which can connect a serial console in Tinkercad to MQTT.',
+  version: '1.0.0',
   action: {
     default_popup: 'index.html',
   },
-  // @ts-ignore
+  // @ts-ignore - Since the type definitions of crxjs are not accessible we need to ignore the error
   background,
-  permissions: [
-    'activeTab',
-    'tabs',
-    'storage',
-  ],
+  permissions: ['activeTab', 'tabs', 'storage'],
   content_scripts: [
     {
       js: ['src/content/index.ts'],
-      matches: ['https://*/*', 'http://*/*'],
+      matches: [
+        'https://www.tinkercad.com/things/*',
+        'http://www.tinkercad.com/things/*',
+      ],
     },
   ],
   host_permissions: ['<all_urls>'],
--- src/content/index.ts
+++ src/content/index.ts
@@ -1,7 +1,62 @@
 import browser from "webextension-polyfill";
 
-browser.storage.local.set({
-  [window.location.hostname]: document.title,
-}).then(() => {
-  browser.runtime.sendMessage(`Saved document title for ${window.location.hostname}`);
+let serial_output: HTMLDivElement;
+let serial_input: HTMLInputElement;
+let serial_send: HTMLAnchorElement;
+
+const observer = new MutationObserver(() => {
+  let text = serial_output.textContent;
+
+  if (!text) {
+    return;
+  }
+
+  // split the serial output by new line
+  let serial_data = text.split("\r\n");
+
+  //get last element of the array
+  let last_serial_data = serial_data[serial_data.length - 2];
+  console.log(last_serial_data);
+  browser.runtime.sendMessage(last_serial_data); // TODO: decide on a message format
+});
+
+let interval = setInterval(() => {
+  serial_output = <HTMLDivElement>(
+    Array.from(
+      document.getElementsByClassName("js-code_panel__serial__text")
+    ).find((e) => e instanceof HTMLDivElement)
+  );
+
+  serial_input = <HTMLInputElement>(
+    Array.from(
+      document.getElementsByClassName("js-code_panel__serial__input")
+    ).find((e) => e instanceof HTMLInputElement)
+  );
+
+  serial_send = <HTMLAnchorElement>(
+    Array.from(
+      document.getElementsByClassName("js-code_panel__serial__send")
+    ).find((e) => e instanceof HTMLAnchorElement)
+  );
+
+  if (serial_output && serial_input && serial_send) {
+    observer.observe(serial_output, {
+      childList: true,
+    });
+
+    stopInterval();
+  }
+}, 500);
+
+function stopInterval() {
+  clearInterval(interval);
+}
+
+// TODO: decide on a message format
+browser.runtime.onMessage.addListener((message) => {
+  console.log(message);
+  if (serial_input && serial_send) {
+    serial_input.value = message;
+    serial_send.click();
+  }
 });
